1acadc030661bbd937445dc5ed05790db56304e2
tomasgons <tomasgons@gmail.com>
2022-01-18 19:53:46 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import BudgetCard from "./components/BudgetCard";
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { Button, Stack } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  return <Container className='my-4'>
+    <Stack direction='horizontal' gap='2' className='mb-4'>
+      <h1 className='me-auto'>Budgets</h1>
+      <Button variant='primary'>Add Budget</Button>
+      <Button variant='outline-primary'>Add Expense</Button>
+    </Stack>
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr)', gap: '1rem', alignItems: 'flex-start' }}>
+      <BudgetCard name='Entertainment' amount={20} max={1000} >
+
+      </BudgetCard>
     </div>
-  );
+
+  </Container>
+
 }
 
 export default App;
---
+++ src/components/BudgetCard.js
@@ -0,0 +1,32 @@
+import { Card, ProgressBar } from 'react-bootstrap'
+import { currencyFormatter } from '../utils'
+
+export default function BudgetCard({ name, amount, max }) {
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title className='d-flex justify-content-between
+                align-items-baseline fw-normal mb-3'>
+                    <div className='me-2'>{name}</div>
+                    <div className='d-flex align-items-baseline'>
+                        {currencyFormatter.format(amount)}
+                        <span className='text-muted fs-6 ms-1'> / {currencyFormatter.format(max)}</span>
+                    </div>
+
+                </Card.Title>
+                <ProgressBar className='rounded-pill'
+                    variant={getProgressBarVariant(amount, max)}
+                    min={0}
+                    max={max}
+                    now={amount} />
+            </Card.Body>
+        </Card>
+    )
+}
+function getProgressBarVariant(amount, max) {
+    const ratio = amount / max
+    if (ratio < .5) return 'primary'
+    if (ratio < .75) return 'warning'
+    return 'danger'
+
+}
